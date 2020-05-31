a8ca17ecd2c5360440f8efbd1d2885709de354d7
Yonnie-Kim <53686910+Yonnie-Kim@users.noreply.github.com>
2020-05-31 21:49:37 +0900

    Create 3-2회차 복습-2.py

---
+++ practice/3-2회차 복습-2.py
@@ -0,0 +1,52 @@
+# 터미널에서 pip install bs4
+#  - bs4: (beautifulsoup4) -> HTML 파싱을 편리하게.
+
+
+import requests
+from bs4 import BeautifulSoup
+
+# requests: HTTP for Humans
+# https://rednooby.tistory.com/97 참고
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(
+    'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
+# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
+
+# select를 이용해서, tr들을 불러오기
+# old_content > table > tbody > tr:nth-child(2)
+movies = soup.select('#old_content > table > tbody > tr')
+print(len(movies))
+
+# old_content > table > tbody > tr:nth-child(2) > td.point
+# old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
+rank = 0
+for movie in movies:
+    a_tag = movie.select_one('td.title > div > a')
+    if a_tag is not None:
+        rank = rank + 1
+        title = a_tag.text
+        star = movie.select_one('td.point').text
+        doc = {
+            'rank': rank,
+            'title': title,
+            'star': star
+        }
+        db.movies.insert_one(doc)
+
+target_movie = db.movies.find_one({'title': '매트릭스'})
+print(target_movie['star'])
+
+target_star = target_movie['star']
+t_movies = list(db.movies.find({'star': target_star}))
+print(len(t_movies))
