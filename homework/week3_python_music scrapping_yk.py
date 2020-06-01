1a91735e6ac7cc01a697a6816b52b6cec619edca
Yonnie-Kim <53686910+Yonnie-Kim@users.noreply.github.com>
2020-06-01 19:28:20 +0900

    Update week3_python_music scrapping_yk.py
    추출&정리한 내용을 db에 저장하기 (songs)

--- homework/week3_python_music scrapping_yk.py
+++ homework/week3_python_music scrapping_yk.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
 # headers에 있는 브라우저 정보들을 제대로 안 넣으면 보안문제로 정보를 모두 불러오지 않으려고 한다.
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -27,3 +31,6 @@ for song in songs:
     singer = song.select_one('td.info > a.artist.ellipsis').text
 
     print(rank, title, singer)
+
+    song = {'rank': rank, 'title': title, 'singer': singer}
+    db.songs.insert_one(song)  # 추출한 내용 DB에 저장하기
